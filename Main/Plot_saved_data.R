1135d6dec77eb859cca3da575273098d20f35651
Grosklos <Guen.Grosklos@umt.edu>
2022-08-10 10:09:58 -0600

    Use full save data for plots

--- Main/Plot_saved_data.R
+++ Main/Plot_saved_data.R
@@ -16,7 +16,7 @@ cam.dist.labels.caps <- c("Random","Slow","Medium","Fast")
 
 # Define landscape variations
 # 1: all slow, 2: all medium, 3: all fast, 4: equal slow, medium, fast 5: 80% fast
-lv.all <- 4
+lv.all <- 3
 lv.labels <- c("_slow_lscape_all","_med_lscape_all","_fast_lscape_all","","_fast_lscape")
 
 for (cam.dist.set in cs.all){
@@ -27,46 +27,61 @@ means_label <- paste(cam.dist.labels[cam.dist.set],"_cams_means",lv.labels[lscap
 props_label <- paste(cam.dist.labels[cam.dist.set],"_cams_props",lv.labels[lscape_var], sep = "")
 
 
-# # # Save .csv files
+# # # Load .csv files
 D.all.Means.mat <- as.matrix(read.csv(paste(fig_dir,"sim_data/",means_label,".csv", sep = "")))
 D.all.Sds.mat <- as.matrix(read.csv(paste(fig_dir,"sim_data/",means_label,"_sds.csv", sep = "")))
+D.all.Means.mat.all <- as.matrix(read.csv(paste(fig_dir,"sim_data/",means_label,"_all.csv", sep = "")))
+D.all.Sds.mat.all <- as.matrix(read.csv(paste(fig_dir,"sim_data/",means_label,"_SD_all.csv", sep = "")))
 all.props.Means <- as.matrix(read.csv(paste(fig_dir,"sim_data/",props_label,".csv", sep = "")))
 all.props.Sds <- as.matrix(read.csv(paste(fig_dir,"sim_data/",props_label,"_sds.csv", sep = "")))
 
-D.all.Means <- c(as.numeric(D.all.Means.mat[,2:6]))
-D.all.Sds <- c(as.numeric(D.all.Sds.mat[,2:6]))
 all.props.Means <- matrix(as.numeric(all.props.Means[,2:4]),nrow = 9, ncol = 3)
 all.props.Sds <- matrix(as.numeric(all.props.Sds[,2:4]),nrow = 9, ncol = 3)
 rownames(all.props.Means) <- c("ABM (Truth)",leg.props)
 colnames(all.props.Means) <- c("Slow","Medium","fast")
 
-D.all.MLE.Means <- as.numeric(D.all.Means.mat[1,2:6])
-D.all.MLE.cov.Means <- as.numeric(D.all.Means.mat[2,2:6])
-D.all.MCMC.Means <- as.numeric(D.all.Means.mat[3,2:6])
-D.all.MCMC.cov.Means <- as.numeric(D.all.Means.mat[4,2:6])
-D.all.MLE.Sds <- as.numeric(D.all.Sds.mat[1,2:6])
-D.all.MLE.cov.Sds <- as.numeric(D.all.Sds.mat[2,2:6])
-D.all.MCMC.Sds <- as.numeric(D.all.Sds.mat[3,2:6])
-D.all.MCMC.cov.Sds <- as.numeric(D.all.Sds.mat[4,2:6])
+####################################
+# Calculate Summaries
+####################################
+D.all.Means <- colMeans(D.all.Means.mat.all[,2:ncol(D.all.Means.mat.all)])
+# colSds for some reason does not like NA vals
+D.all2 <- D.all.Means.mat.all[,2:ncol(D.all.Means.mat.all)]
+D.all2[is.na(D.all2)] <- 0
+D.all.Sds <- colSds(D.all2)
+D.all.Sds[D.all.Sds == 0] <- NA
 
+D.all.MLE.Means <- D.all.Means[1:5]
+D.all.MLE.Sds <- D.all.Sds[1:5]
+D.all.MLE.cov.Means <- D.all.Means[6:10]
+D.all.MLE.cov.Sds <- D.all.Sds[6:10]
+D.all.MCMC.Means <- D.all.Means[11:15]
+D.all.MCMC.Sds <- D.all.Sds[11:15]
+D.all.MCMC.cov.Means <- D.all.Means[16:20]
+D.all.MCMC.cov.Sds <- D.all.Sds[16:20]
 
 if (any(lv.all == 1:3)) {
   # setEPS()
   # postscript(paste(fig_dir,"figs/",means_label,".eps", sep = ""),width=8,height=5)
   plot(seq(0.7,4.7,by=1), 
-       D.all.MLE.Means, 
-       ylim=c(min(nind,D.all.Means - D.all.Sds,na.rm=T),
-              max(nind,D.all.Means + D.all.Sds,na.rm=T)), 
-       xlim=c(0.5,5.5), 
-       xlab="Method", 
-       ylab="Mean Estimates", 
-       pch=16, cex=1.8, xaxt = "n", cex.lab = 1.5, cex.axis = 1.3)
+       D.all.MLE.Means, ylim=c(min(nind,D.all.Means - D.all.Sds,na.rm=T),
+                               max(nind,D.all.Means + D.all.Sds,na.rm=T)), 
+       xlim=c(0.5,5.5),xlab="Method", 
+       ylab="Mean Estimates", pch=16, cex=1.8, xaxt = "n", cex.lab = 1.5, cex.axis = 1.3)
+  # ylab="Mean Estimates", pch=16, cex=1.8, xaxt = "n", cex.lab = 1.5, cex.axis = 1.3)
+  points(seq(0.9,4.9,by=1),D.all.MLE.cov.Means, col="black", pch=1,cex=1.8)
   points(seq(1.1,5.1,by=1),D.all.MCMC.Means, col="blue", pch=16,cex=1.8)
+  points(seq(1.3,5.3,by=1),D.all.MCMC.cov.Means, col="blue", pch=1,cex=1.8)
   arrows(x0=seq(0.7,4.7,by=1), y0=D.all.MLE.Means-D.all.MLE.Sds, 
          x1=seq(0.7,4.7,by=1), y1=D.all.MLE.Means+D.all.MLE.Sds, 
          code=3, angle=90, length=0.1, col="black", lwd=2)
+  arrows(x0=seq(0.9,4.9,by=1), y0=D.all.MLE.cov.Means-D.all.MLE.cov.Sds, 
+         x1=seq(0.9,4.9,by=1), y1=D.all.MLE.cov.Means+D.all.MLE.cov.Sds, 
+         code=3, angle=90, length=0.1, col="black", lwd=2)
   arrows(x0=seq(1.1,5.1,by=1), y0=D.all.MCMC.Means-D.all.MCMC.Sds, 
          x1=seq(1.1,5.1,by=1), y1=D.all.MCMC.Means+D.all.MCMC.Sds, 
+         code=3, angle=90, length=0.1, col="blue", lwd=2)
+  arrows(x0=seq(1.3,5.3,by=1), y0=D.all.MCMC.cov.Means-D.all.MCMC.cov.Sds, 
+         x1=seq(1.3,5.3,by=1), y1=D.all.MCMC.cov.Means+D.all.MCMC.cov.Sds, 
          code=3, angle=90, length=0.1, col="blue", lwd=2)
   lines(c(0.2,6.8), c(nind,nind), type = "l", lty = 2, lwd = 3.5)
   axis(1, at = c(1:5), labels = leg1, cex.axis = 1.3)
