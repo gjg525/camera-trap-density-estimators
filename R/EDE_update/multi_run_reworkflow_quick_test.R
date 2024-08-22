6fa6047e380ac10ae8cb1cba85ba494f6fc51715
gjg525 <guen.grosklos@speedgoat.io>
2024-08-22 15:39:23 -0600

    Create quick workflow test

---
+++ R/EDE_update/multi_run_reworkflow_quick_test.R
@@ -0,0 +1,239 @@
+library(tidyverse)
+library(RColorBrewer)
+library(lattice)
+library(gridExtra)
+library(doParallel)
+
+source("./R/EDE_update/utils.R")
+source("./R/EDE_update/plot_funs.R")
+source("./R/EDE_update/ABM_sim.R")
+source("./R/EDE_update/MCMC_functions.R")
+source("./R/EDE_update/Create_landscape.R")
+source("./R/EDE_update/Collect_data.R")
+source("./R/EDE_update/Collect_tele_data.R")
+
+# Initializations
+fig_colors <- c("#2ca25f", "#fc8d59", "#67a9cf", "#f768a1", "#bae4b3", "#fed98e")
+options(ggplot2.discrete.colour = fig_colors)
+options(ggplot2.discrete.fill = fig_colors)
+
+# Study design
+study_design <- tibble::tibble(
+  q = 30^2, # Number grid cells
+  dx = 1,  # Grid cell lengths
+  dy = 1,
+  t_steps = 500, # Number of time steps (3000 * 15 min ~ 31.25 days)
+  dt = 1,     # Time step size
+  bounds = list(c(0, dx * q ^ 0.5)), # Sampling area boundaries
+  num_groups = 100,
+  group_sizes = list(rep(1, num_groups)),
+  group_spread = 0, # Tightness of grouping behavior (relative to grid size)
+  tot_animals = sum(unlist(group_sizes)),
+  h_range_strength = NA,  # Home range size (not to scale)
+  activity_sync = "sync",
+  activity_prob = list(rep(1, t_steps)), # can be defined for all time steps
+  # MCMC parms
+  num_runs = 100,
+  n_iter = 5000,
+  burn_in = 3000,
+  # staying time censors
+  t_censor = 10, 
+  # num_occ = t_steps / TTE_censor,
+  run_models = list(1:5),
+  covariate_labels = list(c("Slow", "Medium", "Fast"))
+)
+
+# Landscape design
+lscape_design <- tibble::tibble(
+  lscape_tag = "Random",
+  default_kappa = 0, # Turning angle
+  num_roads = 0,
+  Speed_ID = list(c("Slow", "Medium", "Fast")),
+  Speed_mins = list(c(.05, .3, .9)),
+  Speed_maxes = list(c(.15, .5, 1.1)),
+  Trail_ID = list(c("On Trail", "Off Trail")),
+  Trail_speed = list(c("Medium", "Medium"))
+)
+
+# Cam designs
+cam_design <- tibble::tibble(
+  ncam = 500,
+  Design = "Random",
+  Props = list(c(1, 1, 1)), # proportion of cameras placed on and off roads
+  # Design = "Bias",
+  # Props = list(c(0.8, 0.1, 0.1)), # proportion of cameras placed on and off roads
+  # Design = "Bias",
+  # Props = list(c(0, 0, 1)), # proportion of cameras placed on and off roads
+  cam_length = study_design$dx * 0.3, # length of all viewshed sides
+  cam_A = cam_length ^ 2 / 2,
+  tot_snaps = ncam * study_design$t_steps
+)
+
+num_models <- length(unlist(study_design$run_models))
+
+# Initialize summary matrices
+D.all <- tibble::tibble(
+  iteration = rep(1:study_design$num_runs, each = num_models),
+  Model = NA,
+  Covariate = NA,
+  Est = NA,
+  SD = NA,
+  all_results = NA
+)
+
+save_animal_data <- tibble::tibble(
+  iteration = 1:study_design$num_runs,
+  data = NA
+)
+
+all_data <- tibble::tibble(
+  iteration = 1:study_design$num_runs,
+  cam_captures = NA,
+  count_data = NA,
+  encounter_data = NA,
+  stay_time_all = NA,
+  stay_time_raw = NA,
+  stay_time_data = NA,
+  TTE_data_all = NA,
+  TTE_data_raw = NA,
+  TTE_data = NA,
+  STE_data = NA
+)
+
+# # Create custom landscape (tag = "grid", "circ", "squares", "metapop")
+lscape_defs <- lscape_creator(study_design, lscape_design)
+
+# Calculate total area using available space
+study_design <- study_design %>%
+  dplyr::mutate(
+    tot_A = unique(
+      study_design %>%
+        dplyr::reframe(
+          bounds = unlist(bounds),
+          tot_A = (bounds[2] - bounds[1])^2 * sum(!is.na(lscape_defs$Road)) / q
+        ) %>%
+        pull(tot_A)
+    )
+  )
+
+# # camera data summaries
+cam_design <- cam_design %>%
+  dplyr::mutate(percent_cam_coverage = ncam * cam_A / study_design$tot_A)
+
+# Create covariate matrix with 0, 1 values
+study_design <- study_design %>% 
+  dplyr::mutate(
+    num_covariates = length(unlist(covariate_labels)),
+    Z = list(create_covariate_mat(
+      lscape_defs,
+      study_design,
+      unlist(covariate_labels)))
+  )
+dd_og <- c()
+dd <- c()
+# Multi-run simulations
+  # Run agent-based model
+  animalxy.all <- ABM_sim(study_design,
+                          lscape_defs)
+  
+for (run in 1:study_design$num_runs) {
+  print(paste("Run", run, "of", study_design$num_runs))
+  
+  # If running new ABM simulation on each run, save
+  # save_animal_data$data[run] <- list(animalxy.all)  
+  
+  # Place cameras on study area
+  cam_locs <- create_cam_samp_design(study_design,
+                                     lscape_defs,
+                                     cam_design)
+  
+  ################################
+  # Collect data
+  ################################
+  "%notin%" <- Negate("%in%")
+  all_data$cam_captures[run] <- list(get_cam_captures(animalxy.all %>%
+                                                        dplyr::filter(t != 0)))
+  # cam_captures <- get_cam_captures(animalxy)
+
+  stay_time_tele <- Collect_tele_data(animalxy.all, study_design, cam_locs)
+
+  stay_time_summary <- stay_time_tele %>%
+    dplyr::group_by(speed) %>%
+    dplyr::summarise(
+      # cell_mu = mean(t_stay),
+      # cell_sd = sd(t_stay),
+      cam_mu = mean(t_stay * cam_design$cam_A / (study_design$dx * study_design$dx)),
+      cam_sd = sd(t_stay * cam_design$cam_A / (study_design$dx * study_design$dx)),
+      .groups = 'drop'
+    ) %>%
+    dplyr::mutate(stay_prop = cam_mu / sum(cam_mu)) %>%
+    dplyr::rename(Speed = speed) %>%
+    dplyr::arrange(desc(Speed))
+
+  kappa.prior.mu.tdst <- log(stay_time_summary$cam_mu)
+  kappa.prior.var.tdst <- log(stay_time_summary$cam_sd)
+
+  habitat_summary <- lscape_defs %>%
+    dplyr::group_by(Speed) %>%
+    dplyr::summarise(
+      n_lscape = dplyr::n()
+    ) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(
+      prop_lscape = n_lscape / sum(n_lscape)
+    ) %>%
+    dplyr::left_join(
+      cam_locs %>%
+        dplyr::group_by(Speed) %>%
+        dplyr::summarise(
+          # mean_count = mean(count, na.rm = T),
+          ncams = dplyr::n(),
+          .groups = 'drop'
+        ),
+      by = dplyr::join_by(Speed)
+    ) %>%
+    dplyr::left_join(
+      stay_time_summary,
+      by = dplyr::join_by(Speed)
+    )  %>%
+    dplyr::mutate(
+      prop_cams = ncams / sum(ncams, na.rm = T),
+      # stay_prop_full = stay_prop / sum(stay_prop),
+      d_coeff = n_lscape * prop_cams / stay_prop / (cam_design$cam_A * study_design$t_steps)
+    ) %>%
+    dplyr::arrange(desc(Speed)) %>%
+    replace(is.na(.), 0)
+  #
+  cc <- get_count_data(cam_locs, all_data$cam_captures[[run]])
+
+  n_adj <- habitat_summary %>%
+    dplyr::left_join(
+      cc %>%
+        dplyr::group_by(Speed) %>%
+        dplyr::summarise(
+          mean_count = mean(count, na.rm = T),
+          .groups = 'drop'
+        ),
+      by = dplyr::join_by(Speed)
+    ) %>%
+    dplyr::mutate(
+      mean_count = tidyr::replace_na(mean_count, 0),
+      ncams = tidyr::replace_na(ncams, 0)
+    ) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(
+      # stay_prop_adj = 1 / stay_prop * ncams / sum(ncams, na.rm = T),
+      n_lscape = mean_count * n_lscape / (cam_design$cam_A * study_design$t_steps),
+      n_habitat = mean_count * d_coeff
+      # n_full = n_lscape / stay_prop
+      # big_D = mean_count * sum(stay_prop) / stay_prop
+    ) %>%
+    dplyr::select(Speed, prop_lscape, mean_count, ncams, cam_mu, stay_prop, prop_cams, d_coeff, n_lscape, n_habitat)
+
+  dd_og[run] <- mean(cc$count)  * study_design$tot_A / (cam_design$cam_A * study_design$t_steps)
+  
+  dd[run] <- sum(n_adj$n_habitat)
+
+  
+}
+
