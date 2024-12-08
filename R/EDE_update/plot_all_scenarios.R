af0af9f08f37c194f46a1d70648828dcbed8b3cf
gjg525 <guen.grosklos@speedgoat.io>
2024-12-08 08:10:16 -0700

    Update plots

--- R/EDE_update/plot_all_scenarios.R
+++ R/EDE_update/plot_all_scenarios.R
@@ -3,6 +3,16 @@ tot_animals <- 100
 
 fig_colors <- c("#2ca25f", "#fc8d59", "#67a9cf", "#f768a1", "#bae4b3", "#fed98e")
 
+file_names <- c(
+  "random",
+  "slow",
+  "med",
+  "fast",
+  "all_slow",
+  "all_med",
+  "all_fast"
+)
+################################################################################
 # loadRData <- function(fileName){
 #   #loads an RData file, and returns it
 #   load(fileName)
@@ -12,33 +22,26 @@ fig_colors <- c("#2ca25f", "#fc8d59", "#67a9cf", "#f768a1", "#bae4b3", "#fed98e"
 # source("./R/EDE_update/utils.R")
 # source("./R/EDE_update/plot_funs.R")
 # 
-# file_names <- c(
-#   "random",
-#   "slow",
-#   "med",
-#   "fast",
-#   "all_slow",
-#   "all_med",
-#   "all_fast"
-# )
 # 
 # D_all <- tibble::tibble()
-# for (ii in 3:length(file_names)) {
+# for (ii in 1:length(file_names)) {
 #   print(ii)
-#   all_results <- loadRData(paste0("Sim_results/results_", file_names[ii], "_cam.RData"))
-#   
+#   all_results <- loadRData(paste0("G:/My Drive/Missoula_postdoc/TDST_Model/Data/results_",
+#                                   file_names[ii], 
+#                                   "_cam_alt.RData"))
+# 
 #   D_all <- dplyr::bind_rows(
 #     D_all,
-#     all_results[[6]] %>% 
+#     all_results[[5]] %>%
 #       dplyr::mutate(
 #         SampDesign = paste0(file_names[ii], "_cam")
 #       )
 #   )
 # }
 # 
-# save(D_all, file = "Sim_results/D_all.RData")
+# save(D_all, file = "G:/My Drive/Missoula_postdoc/TDST_Model/Data/D_all.RData")
 
-load("Sim_results/D_all.RData")
+load("G:/My Drive/Missoula_postdoc/TDST_Model/Data/D_all.RData")
 
 D_all <- D_all %>% 
   dplyr::mutate(
@@ -51,6 +54,82 @@ D_all <- D_all %>%
       .default = Model
     )
   )
+
+for (ff in 1:length(file_names)) {
+  D_all %>% 
+    dplyr::filter(Est < 250) %>%
+    dplyr::filter(SampDesign == paste0(file_names[ff], "_cam")) %>% 
+    ggplot2::ggplot(ggplot2::aes(x = Model, y = Est, fill = Model)) +
+    ggplot2::geom_boxplot(lwd = 0.5, fatten = .5, outlier.size = 1) +
+    ggplot2::geom_hline(yintercept=tot_animals, linetype="dashed", size = 0.7) +
+    ggplot2::labs(x = "Model",
+                  y = "Posterior Means") +
+    ggplot2::scale_fill_manual(values= fig_colors[1:5]) +
+    ggplot2::scale_color_manual(values = c('grey0', 'grey40', 'grey60')) +
+    ggplot2::theme(text = ggplot2::element_text(size = 20),
+                   legend.title = ggplot2::element_blank(),
+                   panel.grid.major = ggplot2::element_blank(),
+                   panel.grid.minor = ggplot2::element_blank(),
+                   panel.background = ggplot2::element_blank(),
+                   axis.line = ggplot2::element_line(colour = "black"),
+                   panel.border = ggplot2::element_rect(colour = "black", fill=NA, size=1),
+                   legend.position = "none",
+                   legend.background = ggplot2::element_blank(),
+                   legend.spacing.y = ggplot2::unit(0, "mm"),
+                   legend.box.background = ggplot2::element_rect(colour = "black"))
+  
+  ggplot2::ggsave(
+    paste0("G:/My Drive/Missoula_postdoc/TDST_Model/imgs/", 
+           file_names[ff],
+           "_cam.png"),
+    plot = ggplot2::last_plot(),
+    # path = file_path,
+    # scale = 1,
+    width = 8,
+    height = 3,
+    # units = c("in", "cm", "mm", "px"),
+    dpi = 600,
+    limitsize = TRUE,
+    bg = NULL
+  )
+  
+  D_all %>% 
+    dplyr::filter(Est < 250) %>%
+    dplyr::filter(SampDesign == paste0(file_names[ff], "_cam")) %>% 
+    ggplot2::ggplot(ggplot2::aes(x = Model, y = SD, fill = Model)) +
+    ggplot2::geom_boxplot(lwd = 0.5, fatten = .5, outlier.size = 1) +
+    ggplot2::labs(x = "Model",
+                  y = "Posterior Means") +
+    ggplot2::scale_fill_manual(values= fig_colors[1:5]) +
+    ggplot2::scale_color_manual(values = c('grey0', 'grey40', 'grey60')) +
+    ggplot2::theme(text = ggplot2::element_text(size = 20),
+                   legend.title = ggplot2::element_blank(),
+                   panel.grid.major = ggplot2::element_blank(),
+                   panel.grid.minor = ggplot2::element_blank(),
+                   panel.background = ggplot2::element_blank(),
+                   axis.line = ggplot2::element_line(colour = "black"),
+                   panel.border = ggplot2::element_rect(colour = "black", fill=NA, size=1),
+                   legend.position = "none",
+                   legend.background = ggplot2::element_blank(),
+                   legend.spacing.y = ggplot2::unit(0, "mm"),
+                   legend.box.background = ggplot2::element_rect(colour = "black"))
+  
+  ggplot2::ggsave(
+    paste0("G:/My Drive/Missoula_postdoc/TDST_Model/imgs/", 
+           file_names[ff],
+           "_cam_SD.png"),
+    plot = ggplot2::last_plot(),
+    # path = file_path,
+    # scale = 1,
+    width = 8,
+    height = 3,
+    # units = c("in", "cm", "mm", "px"),
+    dpi = 600,
+    limitsize = TRUE,
+    bg = NULL
+  )
+  
+}
 
 # D_all %>%
 #   dplyr::filter(Est < 200) %>%
@@ -111,84 +190,84 @@ D_all <- D_all %>%
 #   ) + 
 #   ggplot2::facet_wrap(~ SampDesign, scales = "free", ncol = 2)
 
-D_all %>% 
-  dplyr::filter(Est < 250) %>%
-  dplyr::filter(SampDesign == "random_cam") %>% 
-  ggplot2::ggplot(ggplot2::aes(x = Model, y = Est, fill = Model)) +
-  ggplot2::geom_boxplot(lwd = 0.5, fatten = .5, outlier.size = 1) +
-  ggplot2::geom_hline(yintercept=nind, linetype="dashed", size = 0.7) +
-  ggplot2::labs(x = "Model",
-       y = "Posterior Means") +
-  # scale_fill_manual(values= c(fig_colors_dark[1:5], 
-  #                             fig_colors_med[1:5], 
-  #                             fig_colors_light[1:5])) +
-  ggplot2::scale_fill_manual(values= fig_colors[1:5]) +
-  ggplot2::scale_color_manual(values = c('grey0', 'grey40', 'grey60')) +
-  # ggplot2::annotate("text", x = 5.4, y = max(D.all$Est), 
-  #          label = "a", 
-  #          size = 5) +
-  ggplot2::theme(text = ggplot2::element_text(size = 16),
-        legend.title = ggplot2::element_blank(),
-        panel.grid.major = ggplot2::element_blank(),
-        panel.grid.minor = ggplot2::element_blank(),
-        panel.background = ggplot2::element_blank(),
-        axis.line = ggplot2::element_line(colour = "black"),
-        panel.border = ggplot2::element_rect(colour = "black", fill=NA, size=1),
-        legend.position = "none",
-        legend.background = ggplot2::element_blank(),
-        legend.spacing.y = ggplot2::unit(0, "mm"),
-        legend.box.background = ggplot2::element_rect(colour = "black"))
-
-ggsave(
-  "Abundance_homogeneous_speeds.png",
-  plot = last_plot(),
-  # path = file_path,
-  scale = 1,
-  width = 5,
-  height = 3,
-  # units = c("in", "cm", "mm", "px"),
-  dpi = 600,
-  limitsize = TRUE,
-  bg = NULL
-)
-
-D_all %>% 
-  dplyr::filter(Est < 250) %>%
-  dplyr::filter(SampDesign == "random_cam") %>% 
-  ggplot2::ggplot(ggplot2::aes(x = Model, y = SD, fill = Model)) +
-  ggplot2::geom_boxplot(lwd = 0.5, fatten = .5, outlier.size = 1) +
-  ggplot2::labs(x = "Model",
-                y = "Posterior SDs") +
-  # scale_fill_manual(values= c(fig_colors_dark[1:5], 
-  #                             fig_colors_med[1:5], 
-  #                             fig_colors_light[1:5])) +
-  ggplot2::scale_fill_manual(values= fig_colors[1:5]) +
-  ggplot2::scale_color_manual(values = c('grey0', 'grey40', 'grey60')) +
-  # ggplot2::annotate("text", x = 5.4, y = max(D.all$Est), 
-  #          label = "a", 
-  #          size = 5) +
-  ggplot2::theme(text = ggplot2::element_text(size = 16),
-                 legend.title = ggplot2::element_blank(),
-                 panel.grid.major = ggplot2::element_blank(),
-                 panel.grid.minor = ggplot2::element_blank(),
-                 panel.background = ggplot2::element_blank(),
-                 axis.line = ggplot2::element_line(colour = "black"),
-                 panel.border = ggplot2::element_rect(colour = "black", fill=NA, size=1),
-                 legend.position = "none",
-                 legend.background = ggplot2::element_blank(),
-                 legend.spacing.y = ggplot2::unit(0, "mm"),
-                 legend.box.background = ggplot2::element_rect(colour = "black"))
-
-ggsave(
-  "SD_homogeneous_speeds.png",
-  plot = last_plot(),
-  # path = file_path,
-  scale = 1,
-  width = 5,
-  height = 3,
-  # units = c("in", "cm", "mm", "px"),
-  dpi = 600,
-  limitsize = TRUE,
-  bg = NULL
-)
+# D_all %>% 
+#   # dplyr::filter(Est < 250) %>%
+#   dplyr::filter(SampDesign == "random_cam") %>% 
+#   ggplot2::ggplot(ggplot2::aes(x = Model, y = Est, fill = Model)) +
+#   ggplot2::geom_boxplot(lwd = 0.5, fatten = .5, outlier.size = 1) +
+#   ggplot2::geom_hline(yintercept=tot_animals, linetype="dashed", size = 0.7) +
+#   ggplot2::labs(x = "Model",
+#        y = "Posterior Means") +
+#   # scale_fill_manual(values= c(fig_colors_dark[1:5], 
+#   #                             fig_colors_med[1:5], 
+#   #                             fig_colors_light[1:5])) +
+#   ggplot2::scale_fill_manual(values= fig_colors[1:5]) +
+#   ggplot2::scale_color_manual(values = c('grey0', 'grey40', 'grey60')) +
+#   # ggplot2::annotate("text", x = 5.4, y = max(D.all$Est), 
+#   #          label = "a", 
+#   #          size = 5) +
+#   ggplot2::theme(text = ggplot2::element_text(size = 16),
+#         legend.title = ggplot2::element_blank(),
+#         panel.grid.major = ggplot2::element_blank(),
+#         panel.grid.minor = ggplot2::element_blank(),
+#         panel.background = ggplot2::element_blank(),
+#         axis.line = ggplot2::element_line(colour = "black"),
+#         panel.border = ggplot2::element_rect(colour = "black", fill=NA, size=1),
+#         legend.position = "none",
+#         legend.background = ggplot2::element_blank(),
+#         legend.spacing.y = ggplot2::unit(0, "mm"),
+#         legend.box.background = ggplot2::element_rect(colour = "black"))
+# 
+# ggsave(
+#   "Abundance_homogeneous_speeds.png",
+#   plot = last_plot(),
+#   # path = file_path,
+#   scale = 1,
+#   width = 5,
+#   height = 3,
+#   # units = c("in", "cm", "mm", "px"),
+#   dpi = 600,
+#   limitsize = TRUE,
+#   bg = NULL
+# )
+# 
+# D_all %>% 
+#   dplyr::filter(Est < 250) %>%
+#   dplyr::filter(SampDesign == "random_cam") %>% 
+#   ggplot2::ggplot(ggplot2::aes(x = Model, y = SD, fill = Model)) +
+#   ggplot2::geom_boxplot(lwd = 0.5, fatten = .5, outlier.size = 1) +
+#   ggplot2::labs(x = "Model",
+#                 y = "Posterior SDs") +
+#   # scale_fill_manual(values= c(fig_colors_dark[1:5], 
+#   #                             fig_colors_med[1:5], 
+#   #                             fig_colors_light[1:5])) +
+#   ggplot2::scale_fill_manual(values= fig_colors[1:5]) +
+#   ggplot2::scale_color_manual(values = c('grey0', 'grey40', 'grey60')) +
+#   # ggplot2::annotate("text", x = 5.4, y = max(D.all$Est), 
+#   #          label = "a", 
+#   #          size = 5) +
+#   ggplot2::theme(text = ggplot2::element_text(size = 16),
+#                  legend.title = ggplot2::element_blank(),
+#                  panel.grid.major = ggplot2::element_blank(),
+#                  panel.grid.minor = ggplot2::element_blank(),
+#                  panel.background = ggplot2::element_blank(),
+#                  axis.line = ggplot2::element_line(colour = "black"),
+#                  panel.border = ggplot2::element_rect(colour = "black", fill=NA, size=1),
+#                  legend.position = "none",
+#                  legend.background = ggplot2::element_blank(),
+#                  legend.spacing.y = ggplot2::unit(0, "mm"),
+#                  legend.box.background = ggplot2::element_rect(colour = "black"))
+# 
+# ggsave(
+#   "SD_homogeneous_speeds.png",
+#   plot = last_plot(),
+#   # path = file_path,
+#   scale = 1,
+#   width = 5,
+#   height = 3,
+#   # units = c("in", "cm", "mm", "px"),
+#   dpi = 600,
+#   limitsize = TRUE,
+#   bg = NULL
+# )
 
